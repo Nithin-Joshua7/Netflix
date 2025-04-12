02b4dd754720839f3d255971440f6f327f38972f
Nithin Joshua <nithinjoshua11@gmail.com>
2025-04-12 10:10:36 +0530

    Update authUser.js

--- frontend/store/authUser.js
+++ frontend/store/authUser.js
@@ -11,7 +11,7 @@ export const useAuthStore = create((set) => ({
 	signup: async (credentials) => {
 		set({ isSigningUp: true });
 		try {
-			const response = await axios.post("https://netflix-clone-mf2c.onrender.com/api/v1/auth/signup", credentials,{ withCredentials: true });
+			const response = await axios.post("https://netflixb.onrender.com/api/v1/auth/signup", credentials,{ withCredentials: true });
 			set({ user: response.data.user, isSigningUp: false });
 			toast.success("Account created successfully");
 		} catch (error) {
@@ -22,7 +22,7 @@ export const useAuthStore = create((set) => ({
 	login: async (credentials) => {
 		set({ isLoggingIn: true });
 		try {
-			const response = await axios.post("https://netflix-clone-mf2c.onrender.com/api/v1/auth/login", credentials,{ withCredentials: true });
+			const response = await axios.post("https://netflixb.onrender.com/api/v1/auth/login", credentials,{ withCredentials: true });
 			set({ user: response.data.user, isLoggingIn: false });
 		} catch (error) {
 			set({ isLoggingIn: false, user: null });
@@ -32,7 +32,7 @@ export const useAuthStore = create((set) => ({
 	logout: async () => {
 		set({ isLoggingOut: true });
 		try {
-			await axios.post("https://netflix-clone-mf2c.onrender.com/api/v1/auth/logout",{}, { withCredentials: true });
+			await axios.post("https://netflixb.onrender.com/api/v1/auth/logout",{}, { withCredentials: true });
 			set({ user: null, isLoggingOut: false });
 			toast.success("Logged out successfully");
 		} catch (error) {
