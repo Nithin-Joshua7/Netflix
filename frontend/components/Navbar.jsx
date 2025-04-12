fdacbcc1e184f62f6d9581b9e86b36a8f2c25e9f
Nithin Joshua <nithinjoshua11@gmail.com>
2025-04-12 10:55:17 +0530

    Update Navbar.jsx

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -8,39 +8,38 @@ function Navbar() {
   const togglemenu = () => setMobileMenuOpen(!isMobilemenuOpen);
 
   return (
-    <header className='max-w-6xl mx-auto flex items-center justify-between p-4 h-20 relative'>
+    <>
+      <header className='max-w-6xl mx-auto flex items-center justify-between p-4 h-20 relative'>
 
-      {/* Left: Logo */}
-      <div className='flex items-center z-50'>
-        <Link to='/'>
-          <img src="/netflix-logo.png" alt="netflix-logo" className='w-32 sm:w-40' />
-        </Link>
+        {/* Left: Logo */}
+        <div className='flex items-center z-50'>
+          <Link to='/'>
+            <img src="/netflix-logo.png" alt="netflix-logo" className='w-32 sm:w-40' />
+          </Link>
+        </div>
+
+        {/* Right: Logout Button (Desktop only) */}
+        <div className='hidden sm:block z-50'>
+          <button
+            onClick={logout}
+            className='bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg transition'
+          >
+            Logout
+          </button>
+        </div>
+      </header>
+
+      {/* Centered Text Section */}
+      <div className="flex items-center justify-center h-screen bg-black">
+        <h1 className='text-white text-3xl sm:text-4xl font-semibold'>
+          This is Home Page
+        </h1>
       </div>
-
-     
-     
-
-      {/* Right: Logout Button (Desktop only) */}
-      <div className='hidden sm:block z-50'>
-        <button
-          onClick={logout}
-          className='bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg transition'
-        >
-          Logout
-        </button>
-      </div>
-</header>
-       <div className="flex items-center justify-center h-screen bg-black">
-      <h1 className='text-white text-3xl sm:text-4xl font-semibold'>
-        This is Home Page
-      </h1>
-    </div>
 
       {/* Mobile Dropdown */}
       {isMobilemenuOpen && (
         <div className='w-full sm:hidden mt-4 z-40 bg-black border rounded border-gray-800 absolute top-full left-0'>
           <div className='flex flex-col gap-2 p-4'>
-
             <button
               onClick={logout}
               className='bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg transition'
@@ -50,7 +49,7 @@ function Navbar() {
           </div>
         </div>
       )}
-   
+    </>
   );
 }
 
