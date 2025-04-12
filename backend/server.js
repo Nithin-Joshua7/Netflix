1ccb3db484407e56aab135d65bd28c02d30a2867
Nithin Joshua <nithinjoshua11@gmail.com>
2025-04-12 10:21:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,16 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 
 const app = express()
+app.use(cors({
+  origin: "https://netflix-ajta.onrender.com",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({
-    origin: "https://netflix-ajta.onrender.com",
-    credentials: true
-}))
 
 
 
